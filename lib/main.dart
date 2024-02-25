a39c11fa40d3a6ccf7f8922a03c1f57fb3413bc9
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-02-25 21:34:04 +0530

    main

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_editor_internship/screens/home_screen.dart';
+
+void main() {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      title: 'Image Editor Flutter',
+      home: const HomeScreen(),
+    );
+  }
+}
